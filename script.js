4ea62b68ccfb4cc935d1f89f5bf6651a223bbe65
KodehodeTor <torjarand.26pg@gmail.com>
2026-06-25 14:04:36 +0200

    Added notes for JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // Calculator program
 
+// Variable for display to use under
 const display = document.getElementById("display");
 
+// Function accepts parameter input and whatever value is passed into the function will be stored in input.
 function appendToDisplay(input) {
   display.value += input;
 }
 
+// Function clears the display by adding nothing to it.
 function clearDisplay() {
   display.value = "";
 }
-
+// Function calculates the inputs by using eval, then catches any error if input is wrong. For example by using a operator after a number without following it, or adding two operators after eachother.
 function calculate() {
   try {
     display.value = eval(display.value);
